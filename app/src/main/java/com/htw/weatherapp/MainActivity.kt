a63fc7d986c158f2add6c381277d3a30a3a7707d
htw1 <henryk.woronow@gmail.com>
2019-08-07 20:10:54 +0200

    Initial commit

---
+++ app/src/main/java/com/htw/weatherapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.htw.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
